d83a8ddc9cd263d027d7f624da714b65dec064a6
Michael Arguello <marguello@uri.edu>
2020-02-05 18:56:40 -0500

    Completed Code

--- Coding Challenge Week 2/Assignment_4.py
+++ Coding Challenge Week 2/Assignment_4.py
@@ -1,1 +1,11 @@
 
+try:
+    age = int(input("What is your age? "))
+except NameError:
+    print("Error: You Must Enter a Number for your age")
+except SyntaxError:
+    print("Error: You Must Enter a Number for your age")
+else:
+    print "Your age is " + str(age)
+    retire = str(65-age)
+    print "Years left until retirement:" , retire, "years"
