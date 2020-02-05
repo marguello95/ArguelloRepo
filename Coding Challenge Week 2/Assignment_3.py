5100ad5c2c27cdb7c01ef9d55e982d7154ef9864
Michael Arguello <marguello@uri.edu>
2020-02-05 18:59:29 -0500

    Completed Code

--- Coding Challenge Week 2/Assignment_3.py
+++ Coding Challenge Week 2/Assignment_3.py
@@ -1,1 +1,13 @@
-# Assign 3
+
+def word_count(str):
+    counts = dict()
+    words = str.split()
+
+    for word in words:
+        if word in counts:
+            counts[word] += 1
+        else:
+            counts[word] = 1
+    return counts
+
+print( 'Number of times each word occurs:' , word_count('hi dee hi how are you mr dee'))
