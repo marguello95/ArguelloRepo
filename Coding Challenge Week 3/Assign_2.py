7be06b38933d4a0545b30e450a1cd77cdd321440
Michael Arguello <marguello@uri.edu>
2020-02-12 19:11:32 -0500

    Completed Code

--- Coding Challenge Week 3/Assign_2.py
+++ Coding Challenge Week 3/Assign_2.py
@@ -1,1 +1,12 @@
+import sys
 
+#Creates name as empy and will then fill with input from .bat file
+#Will allow for 3 names to be input into the .bat file
+name = []
+name.append(sys.argv[1])
+name.append(sys.argv[2])
+name.append(sys.argv[3])
+
+#Prints out number of letters of each name, as well as the name that was input
+for x in name:
+    print("There are " + str(len(x)) + " letters in the name " + str(x))
