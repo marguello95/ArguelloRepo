6fb406bb2a1842e29f2831b313ed395e3bb75620
Michael Arguello <marguello@uri.edu>
2020-04-21 14:39:36 -0400

    Final Code

--- Week_9/CC_9.py
+++ Week_9/CC_9.py
@@ -1,3 +1,4 @@
+# import all needed below
 import arcpy,os
 from math import radians, sin, cos
 
@@ -100,7 +101,42 @@ print "10M buffer created..."
 # Select layer by location for all lines that pass through the 10 m buffer. This will allow code to select only the
 # lines that originate from the site locations only, not segmants that cross land twice
 
-##PROBLEM HERE??
-arcpy.SelectLayerByLocation_management(in_layer="Single_to_Multi_out.shp", overlap_type="INTERSECT", select_features="Location_w_10m_Buff.shp",
+# Make a feature layer to use in select by location tool
+arcpy.MakeFeatureLayer_management("Single_to_Multi_out.shp","single_part")
+arcpy.MakeFeatureLayer_management("Location_w_10m_Buff.shp","buffer")
+
+# Using the select Layer by location tool with created layers
+arcpy.SelectLayerByLocation_management(in_layer="single_part", overlap_type="INTERSECT", select_features="buffer",
                                        search_distance="", selection_type="NEW_SELECTION", invert_spatial_relationship="NOT_INVERT")
 
+# Used to create a new shape file from select layer by location tool
+arcpy.CopyFeatures_management("single_part", "Single_to_Multi_out_Select.shp")
+
+
+
+arcpy.MakeFeatureLayer_management("Single_to_Multi_out_Select.shp","SM_Select")
+arcpy.MakeFeatureLayer_management("Location_w_10m_Buff.shp","buffer2")
+
+print "Calculation Mean and Std Deviation for each site... "
+
+# Spatial Join to join Site Code to Lines
+arcpy.SpatialJoin_analysis(target_features="SM_Select", join_features="buffer2", out_feature_class="Final_Lines_w_Site.shp", join_operation="JOIN_ONE_TO_MANY", join_type="KEEP_ALL",
+                           field_mapping='Id "Id" true true false 6 Long 0 6 ,First,#,Single_to_Multi_out_Select,Id,-1,-1;ORIG_FID "ORIG_FID" true true false 10 Long 0 10 ,First,#,Single_to_Multi_out_Select,ORIG_FID,-1,-1;Id_1 "Id_1" true true false 6 Long 0 6 ,First,#,Location_w_10m_Buff,Id,-1,-1;Site_Code "Site_Code" true true false 5 Text 0 0 ,First,#,Location_w_10m_Buff,Site_Code,-1,-1;BUFF_DIST "BUFF_DIST" true true false 19 Double 0 0 ,First,#,Location_w_10m_Buff,BUFF_DIST,-1,-1;ORIG_FID_1 "ORIG_FID_1" true true false 10 Long 0 10 ,First,#,Location_w_10m_Buff,ORIG_FID,-1,-1',
+                           match_option="INTERSECT", search_radius="", distance_field_name="")
+
+
+# Adds a field for length to the Final lines with site codes file
+arcpy.MakeFeatureLayer_management("Final_Lines_w_Site.shp","Fin_Line")
+
+arcpy.AddGeometryAttributes_management(Input_Features="Fin_Line", Geometry_Properties="LENGTH", Length_Unit="METERS", Area_Unit="", Coordinate_System="")
+
+
+# Summary statistics calculates the mean and STDEV for each of the 9 site
+# This is where the mean and standard dev. for each will be calculated and store- see here for final values
+arcpy.MakeFeatureLayer_management("Final_Lines_w_Site.shp","Fin_Line2")
+arcpy.Statistics_analysis(in_table="Fin_Line2", out_table="Mean_STDEV_Table", statistics_fields="LENGTH MEAN;LENGTH STD", case_field="JOIN_FID")
+
+
+print "MEAN AND STDEV CREATED IN DATA FOLDER - SEE (Mean_STDEV_Table) for values "
+print "Row id represents the site number: ( 1 = A1 / 2 = A2 / 3 = A3 / 4 = B1 / 5 = B2 / 6 = B3 / 7 = C1 / 8 = C2 / 9 = C3 )"
+print "Final_Lines_w_Site.shp = Final Radiating Lines Output"
